7c04760520438daa00b347d05cacaadd1adac50a
mblenkhorn <55706741+mblenkhorn@users.noreply.github.com>
2021-12-29 17:00:45 +0000

    Updated LeftMenu.tsx
    Updated LeftMenu for testing and adding in menu items for the diagramming tool.

--- diagram-tool/src/components/LeftMenu.tsx
+++ diagram-tool/src/components/LeftMenu.tsx
@@ -6,19 +6,27 @@ import Divider from '@mui/material/Divider';
 import MenuList from '@mui/material/MenuList';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
 
 import GetAppOutlinedIcon from '@mui/icons-material/GetAppOutlined';
 import AddIcon from '@mui/icons-material/Add';
 
 import Typography from '@mui/material/Typography';
 
+
+function Print(){
+  alert("TEMPLATE");
+}
+
+
 export default function LeftMenu() {
   return (
     <Box>
 
-      <Box sx={{ m: 2 }}>
+      <Box sx={{ m: 3 }}>
       <TrayItemWidget model={{ type: 'in' }} name="In Node" color="rgb(192,255,0)" />
       <TrayItemWidget model={{ type: 'out' }} name="Out Node" color="rgb(0,192,255)" />
+      <TrayItemWidget model={{type: 'in'}} name="In Node" color="rgb(255,0,0)"/>
       </Box>
       <MenuList>
         <Divider />
@@ -30,6 +38,12 @@ export default function LeftMenu() {
           <GetAppOutlinedIcon fontSize="small"/>
           <ListItemText>Export as image</ListItemText>
         </MenuItem>
+        <MenuItem>
+        <a href="documentation.html"><ListItemText color='primary'>Documentation</ListItemText></a>
+        </MenuItem>
+        <MenuItem>
+        <ListItem onClick={Print}>Create Template</ListItem>
+        </MenuItem>
       </MenuList>
-    </Box>);
+    </Box>); 
 }
